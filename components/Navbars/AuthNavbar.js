6d4f77537d5bace93809c7e27da9a3d9f9474e1f
Timothy Austin <timothyonyea@gmail.com>
2023-01-24 22:28:18 +0100

    nav edits

--- components/Navbars/AuthNavbar.js
+++ components/Navbars/AuthNavbar.js
@@ -11,13 +11,8 @@ export default function Navbar(props) {
       <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link href="/">
-              <a
-                className="text-green hover:ease-in-out duration-300 hover:text-white text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-                href="/"
-              >
-                Genius Tutors 
-              </a>
+            <Link href="/">              
+            <span className="text-2xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase">GeniusTutors Academy</span>                
             </Link>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
